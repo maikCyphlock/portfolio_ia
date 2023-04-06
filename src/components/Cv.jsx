fe5f039eff2b86873fbdd4df62d86278d7eb77f1
maikCyphlock <maikolaguilar656@gmail.com>
2023-04-06 14:21:38 -0400

    logic component

---
+++ src/components/Cv.jsx
@@ -0,0 +1,125 @@
+
+
+function Cv() {
+    return (
+        <article class="prose md:prose-lg lg:prose-xl mb-10">
+            <figure className="grid  place-content-center">
+                <img src="/maikol.png" className="w-64 " alt="" />
+            </figure>
+            <h1>Maikol D. Aguilar F.</h1>
+            <p>
+                I'm a junior full stack developer with a passion for technology and a focus on frontend development. I'm a proactive and creative problem solver with a dedication to clean code and efficient business logic. Furthermore, I'm also studying English to improve my language skills.
+            </p>
+            <p >
+                Through my personal projects, I have demonstrated proficiency in React and Next.js, as well as knowledge of Vite, ThreeJS, and Zustand. I have developed a minimal Minecraft clone using these technologies, showcasing my problem-solving skills and ability to utilize library hooks. Additionally, I have created an app with Astro that retrieves data from the backend and deploys the API every hour through GitHub actions. I have also contributed to the Kings League Project by making the rules section and GitHub statuses of the README. Overall, these projects demonstrate my passion for learning and my commitment to constantly improving my skills as a web
+            </p>
+
+            <h2>Work Experience</h2>
+            <div>
+                <h4>Federación Venezolana de Fútbol, Freelance job
+                    2021</h4>
+                <p>
+                    Creating a prototype of Landing page
+                    Submitting information on the website
+                    Deploying on Vercel</p>
+            </div>
+
+
+            <div>
+
+                <h4>
+                    Clon de Mincraft | React Clone de Mincraft | React.
+                    diciembre. 2022
+                </h4>
+                <p>
+
+                    A minimal minecraft clone built with react, vite, threejs, and zustand. Using the three hooks built-in in its library link to repo: <a href="https://github.com/maikCyphlock/minecraft-react">https://github.com/maikCyphlock/minecraft-react</a>
+                </p>
+            </div>
+            <div>
+                <h4>
+
+                    Dolar Ve
+                </h4>
+                <p>
+                    This is an app made with Astro that gets data from the backend: <a href="https://github.com/maikCyphlock/dollar-ve-api">
+                        https://github.com/maikCyphlock/dollar-ve-api </a> that scrapes and retrieves the data to Cloudflare workers and GitHub actions deploy the API every hour.
+                </p>
+                <p>
+                    Link to the frontend repo: <a href="https://github.com/maikCyphlock/dollar-frontend-api">
+                        https://github.com/maikCyphlock/dollar-frontend-api
+                    </a>
+                </p>
+
+            </div>
+
+            <div>
+                <h4>
+                    contributor of the Kings League - OSS project
+                </h4>
+                <p>
+                    I have had the opportunity to participate with midudev's open source project and contribute to make the rules section and the GitHub statuses of the README <a href="https://github.com/midudev/kings-league-project" >
+                        https://github.com/midudev/kings-league-project.
+                    </a>
+                </p>
+            </div>
+
+            <section>
+                <h2>education</h2>
+                <h4>Non-traditional study</h4>
+                <ul>
+                    <li>learning with freeCodeCamp, YouTube and MDN documentation, reading books such as: you don't know JavaScript, and doing think by myself also when I’ve got stuck I search on Stack Overflow and google</li>
+                </ul>
+            </section>
+            <section>
+                <h4>OpenBootCamp</h4>
+                <h5>Full-stack developer</h5>
+                <p>Jan 2022 — Present</p>
+            </section>
+            <section>
+                <h4>UNESR (universidad Nacional experimental Simon rodrigues)</h4>
+                <h5>English bachelor's master degree</h5>
+                <p>Oct 2019 — Present</p>
+            </section>
+            <section>
+                <h4>Hard Skills</h4>
+                <h5>Advance:</h5>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>responsive design</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>react</li>
+                    <li>next.js</li>
+                    <li>Node.js</li>
+                </ul>
+                <h4>Intermediate:</h4>
+                <ul>
+                    <li>Node.js</li>
+                    <li>typescript</li>
+                    <li>MongoDB</li>
+                    <li>cloud hosting (Firebase, Supabase)</li>
+                </ul>
+                <h4>Beginner:</h4>
+                <ul>
+                    <li>Go</li>
+                    <li>Rust</li>
+                </ul>
+            </section>
+            <section>
+                <h4>Soft Skills</h4>
+                <ul>
+                    <li>Proactivity</li>
+                    <li>Creativity</li>
+                    <li>Adaptability</li>
+                    <li>Attention to Detail</li>
+                    <li>Communication</li>
+                </ul>
+            </section>
+
+
+        </article>
+    )
+}
+
+export default Cv
---
+++ src/components/Search.jsx
@@ -0,0 +1,68 @@
+"use client";
+import React,{useEffect, useState} from 'react'
+import useStore from './SearchContext';
+import useOpenAI from './useOpenAI';  
+
+
+function Search() {
+  const text= useStore((state) => state.text);
+  const setText  = useStore((state) => state.setText);
+  const loading = useStore((state) => state.loading);
+  const [setPrompt] = useOpenAI("eres el asistente personal de maikol aguilar y responderas todas las preguntas")
+  
+    
+    const HandlerSubmit = (e) => {
+      e.preventDefault()
+      setPrompt(text)
+  
+    }
+    const HandlerText = (event) => {
+        setText(event.target.value)
+        // setSearchResult(response.choices[0].text)
+      
+    }
+    
+  return (
+    <form className="w-full max-w-lg relative" onSubmit={HandlerSubmit} >
+        <div className="flex items-center bg-white border rounded-lg shadow-lg overflow-hidden p-1 hover:border-1 hover:border-green-300 focus:border-green-300">
+          <input
+            className="appearance-none bg-transparent border-none w-full text-gray-700 m-3 py-2 px-2 leading-tight focus:outline-none text-ellipsis "
+            type="text"
+            placeholder="Search"
+            aria-label="Search"
+            onChange={ (e) => HandlerText(e)}
+            disabled={!loading}
+            value={text}
+          />
+          <button
+            className="flex-shrink-0 text-teal-700  text-sm py-2 px-4 rounded"
+            type="submit"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
+                <g transform="translate(0 -96)">
+                  <g transform="translate(0 96)">
+                    <path
+                      fillRule="nonzero"
+                      d="M24 0v24H0V0h24zM12.594 23.258l-.012.002-.071.035-.02.004-.014-.004-.071-.036c-.01-.003-.019 0-.024.006l-.004.01-.017.428.005.02.01.013.104.074.015.004.012-.004.104-.074.012-.016.004-.017-.017-.427c-.002-.01-.009-.017-.016-.018zm.264-.113l-.014.002-.184.093-.01.01-.003.011.018.43.005.012.008.008.201.092c.012.004.023 0 .029-.008l.004-.014-.034-.614c-.003-.012-.01-.02-.02-.022zm-.715.002a.023.023 0 00-.027.006l-.006.014-.034.614c0 .012.007.02.017.024l.015-.002.201-.093.01-.008.003-.011.018-.43-.003-.012-.01-.01-.184-.092z"
+                    ></path>
+                    <path
+                      fill="currentColor"
+                      d="M17.991 6.01L5.399 10.563l4.195 2.428 3.699-3.7a1 1 0 011.414 1.415l-3.7 3.7 2.43 4.194L17.99 6.01zm.323-2.244c1.195-.433 2.353.725 1.92 1.92l-5.282 14.605c-.434 1.198-2.07 1.344-2.709.241l-3.217-5.558-5.558-3.217c-1.103-.639-.958-2.275.241-2.709l14.605-5.282z"
+                    ></path>
+                  </g>
+                </g>
+              </g>
+            </svg>
+          </button>
+        </div>
+      </form>
+  )
+}
+
+export default Search
