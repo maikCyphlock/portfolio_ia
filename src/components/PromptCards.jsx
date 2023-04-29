e0056373c24f189d89f9bb9e53c9f5069987ce28
maikCyphlock <maikolaguilar656@gmail.com>
2023-04-29 14:42:13 -0400

    changins for better perf

--- src/components/PromptCards.jsx
+++ src/components/PromptCards.jsx
@@ -1,27 +1,48 @@
 import React from 'react'
-import useStore from './SearchContext';
-function PromptCards() {
-    const setText  = useStore((state) => state.setText);
-    const handlerClick = (e) => {
-        setText(e.target.value)
-        
-    }
+import useStore from './SearchContext'
+function PromptCards () {
+  const setText = useStore((state) => state.setText)
+  const handlerClick = (e) => {
+    setText(e.target.value)
+  }
   return (
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-3xl ">
-  <button onClick={(e)=>handlerClick(e) } class="bg-zinc-50 transition-colors hover:bg-slate-100 p-3 m-1 md:p-4 md:m-0 shadow-lg rounded-xl" value={"¿Qué tecnologías has utilizado en tus proyectos personales para desarrollar un clon de Minecraft y una aplicación con Astro?"}>
-  ¿Qué tecnologías has utilizado en tus proyectos personales para desarrollar un clon de Minecraft y una aplicación con Astro?
-  </button>
-  <button onClick={(e)=>handlerClick(e) }  class="bg-zinc-50 transition-colors hover:bg-slate-100 p-3 m-1 md:p-4 md:m-0 shadow-lg  rounded-xl" value={"¿Cómo demuestra habilidad para resolver problemas en tus proyectos?"}>
-  ¿Cómo demuestra habilidad para resolver problemas en tus proyectos?
-  </button>
-  <button onClick={(e)=>handlerClick(e) }  class="bg-zinc-50 transition-colors hover:bg-slate-100 p-1 hidden md:block md:p-4 shadow-lg  rounded-xl" value={"¿Qué contribuciones has hecho al proyecto Kings League?"}>
-  ¿Qué contribuciones ha hecho al proyecto Kings League?
-  </button>
-  <button onClick={(e)=>handlerClick(e) }  class="bg-zinc-50 transition-colors hover:bg-slate-100 p-1 hidden md:block md:p-4 shadow-lg  rounded-xl" value={"¿Cómo te describirías como desarrollador web, sabe más de un idioma? "}>
-  ¿Cómo te describirías como desarrollador web, sabe más de un idioma? 
-  </button>
-</div>
-
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-3xl ">
+      <button
+        onClick={(e) => handlerClick(e)}
+        className="bg-zinc-50 transition-colors hover:bg-slate-100 p-3 m-1 md:p-4 md:m-0 shadow-lg rounded-xl"
+        value={
+          '¿Qué tecnologías has utilizado en tus proyectos personales para desarrollar un clon de Minecraft y una aplicación con Astro?'
+        }
+      >
+        ¿Qué tecnologías has utilizado en tus proyectos personales para
+        desarrollar un clon de Minecraft y una aplicación con Astro?
+      </button>
+      <button
+        onClick={(e) => handlerClick(e)}
+        className="bg-zinc-50 transition-colors hover:bg-slate-100 p-3 m-1 md:p-4 md:m-0 shadow-lg  rounded-xl"
+        value={
+          '¿Cómo demuestra habilidad para resolver problemas en tus proyectos?'
+        }
+      >
+        ¿Cómo demuestra habilidad para resolver problemas en tus proyectos?
+      </button>
+      <button
+        onClick={(e) => handlerClick(e)}
+        className="bg-zinc-50 transition-colors hover:bg-slate-100 p-1 hidden md:block md:p-4 shadow-lg  rounded-xl"
+        value={'¿Qué contribuciones has hecho al proyecto Kings League?'}
+      >
+        ¿Qué contribuciones ha hecho al proyecto Kings League?
+      </button>
+      <button
+        onClick={(e) => handlerClick(e)}
+        className="bg-zinc-50 transition-colors hover:bg-slate-100 p-1 hidden md:block md:p-4 shadow-lg  rounded-xl"
+        value={
+          '¿Cómo te describirías como desarrollador web, sabe más de un idioma? '
+        }
+      >
+        ¿Cómo te describirías como desarrollador web, sabe más de un idioma?
+      </button>
+    </div>
   )
 }
 
--- src/components/useOpenAI.js
+++ src/components/useOpenAI.js
@@ -1,47 +1,43 @@
-import { useEffect, useState } from "react";
-import useStore from "./SearchContext";
-
-const useOpenAI = (prmt) => {
-  const setSearchResult = useStore(state=> state.setSearchResult)
-  const setLoading = useStore(state=> state.setLoading)
-  const setError = useStore(state=> state.setError)
-  const [prompt, setPrompt] = useState(prmt);
+import { useEffect, useState } from 'react'
+import useStore from './SearchContext'
+import { API_URL } from '../config'
+const useOpenAI = (prompt) => {
+  const setSearchResult = useStore((state) => state.setSearchResult)
+  const setLoading = useStore((state) => state.setLoading)
+  const setError = useStore((state) => state.setError)
+  const [searchPrompt, setSearchPrompt] = useState(prompt)
 
   useEffect(() => {
-   
-    const options = {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-        "X-RapidAPI-Key": process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-        "X-RapidAPI-Host": "openai80.p.rapidapi.com",
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "assistant", content: `this is the information about maikol douglas aguilar falcon:  is a full stack developer with a strong passion for technology and a focus on frontend development. he possess problem-solving skills with a dedication to clean code and efficient business logic. he have expertise in React and Next.js and knowledge of Vite, ThreeJS, and Zustand, as demonstrated through their personal projects, including a minimal Minecraft clone built with these technologies and an app with Astro that retrieves data from the backend and deploys the API every hour through GitHub actions. he have also contributed to an open-source project as a contributor.  maikol has a non-traditional educational background, mainly self-taught through freeCodeCamp, YouTube, and MDN documentation, supplemented by reading books and searching on Stack Overflow and Google. he are currently pursuing a bachelor's degree in English and have a C1 level of proficiency in English.and learing german in A1 level, maikol possesses strong hard skills in JavaScript, responsive design, HTML, CSS, react, next.js, Node.js, typescript, MongoDB, and cloud hosting, with beginner-level knowledge of Go and Rust. he also have soft skills, including proactivity, creativity, adaptability, attention to detail, and communication, he is 21yo, lives in acarigua,portuguesa,venezuela.|| respond in the same languague as the question and be concise,(only respond question about maikol aguilar without no doubt):${prompt}` }],
-        stream: false,
-      }),
-    };
-  
-
-    const Fetch = ( async() => {
+    const fetchData = async () => {
       try {
-        const response = await fetch('https://openai80.p.rapidapi.com/chat/completions', options);
-        setLoading(true);
-        const data = await response.json();
-        setSearchResult(data);
+        await setLoading(() => true)
+        const response = await fetch(`${API_URL()}/api/chat`, {
+          method: 'POST',
+          body: JSON.stringify({
+            searchPrompt
+          })
+        })
+
+        const data = await response.json()
+
+        if (response.ok) {
+          setSearchResult(data)
+          await setLoading(() => false)
+        } else {
+          setError('Error en la respuesta del servidor' + response.statusText)
+          await setLoading(() => false)
+        }
       } catch (error) {
-        console.error({error});
-        
-        setError('error');
+        setError('Error al llamar al servidor')
+      } finally {
+        await setLoading(false)
       }
-    })()
-    
-    return () => setLoading(false)
+    }
 
-  }, [prompt, setError, setLoading, setSearchResult]);
+    fetchData()
+  }, [searchPrompt, setError, setLoading, setSearchResult])
 
-  return [ setPrompt];
-};
+  return [setSearchPrompt]
+}
 
-export default useOpenAI;
+export default useOpenAI
